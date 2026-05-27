0e3e980308addcca78051ac3e0a34b138e0d7ad3
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-27 16:18:25 +0530

    Create Stateless Widget Of Custom Text Feild

---
+++ lib/cutstomtextfeild/customtextfeild.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Customtextfeild extends StatelessWidget {
+  const Customtextfeild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

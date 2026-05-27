dc45e13fb131ef75fd5fd4e2b334388f1f9166e9
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-27 16:21:48 +0530

    Practice github commands

--- lib/cutstomtextfeild/customtextfeild.dart
+++ lib/cutstomtextfeild/customtextfeild.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+
 class Customtextfeild extends StatelessWidget {
   const Customtextfeild({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder()
+      ),
+    );
   }
 }
